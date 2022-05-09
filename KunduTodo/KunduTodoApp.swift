98777dde1acfcd6e84e0a6e59e878db8fecec697
Blaine <blainesolomon11@gmail.com>
2022-05-09 15:56:28 -0700

    Initial Commit

---
+++ KunduTodo/KunduTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KunduTodoApp.swift
+//  KunduTodo
+//
+//  Created by Blaine on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct KunduTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
